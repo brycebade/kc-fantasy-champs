fce5b76363f0031e27d83b63624d68d9df3deb69
Bryce Bade <229314181+brycebade@users.noreply.github.com>
2026-05-13 19:57:39 +0000

    began building teams-data.js to incorporate teams into js

---
+++ teams-data.js
@@ -0,0 +1,74 @@
+export const teams = [
+    {
+        id: "team1",
+        name: "Average Joe's",
+        owner: "Jared Bade",
+        logo: ""
+    },
+    {
+        id: "team2",
+        name: "Competitive Pricks",
+        owner: "Aaron Bade",
+        logo: ""
+    },
+    {
+        id: "team3",
+        name: "Good Juju",
+        owner: "Julie Stokes",
+        logo: ""
+    },
+    {
+        id: "team4",
+        name: "Here So I Won't Get Fined",
+        owner: "Ashley Riley",
+        logo: ""
+    },
+    {
+        id: "team5",
+        name: "Kelce-3PO",
+        owner: "Bryce Bade Jr",
+        logo: ""
+    }
+    {
+        id: "team6",
+        name: "Krafty motherTuckers",
+        owner: "Cayt McAnallen",
+        logo: ""
+    },
+    {
+        id: "team7",
+        name: "Pack Attack",
+        owner: "Cassie McAnallen",
+        logo: ""
+    },
+    {
+        id: "team8",
+        name: "Patrick Is Ma'Homie",
+        owner: "Bryce Bade",
+        logo: ""
+    },
+    {
+        id: "team9",
+        name: "Redzone Kings",
+        owner: "Andrew Drape",
+        logo: ""
+    },
+    {
+        id: "team10",
+        name: "Schmuck Face",
+        owner: "Robert Snook",
+        logo: ""
+    },
+    {
+        id: "team11",
+        name: "Slick's",
+        owner: "Zach Schlichting",
+        logo: ""
+    },
+    {
+        id: "team12",
+        name: "Victorious Secret",
+        owner: "Crystal Bade",
+        logo: ""
+    }
+]
