0153973de7527c503ff2673028aba164b46e238a
brycebade <bryce.bade@gmail.com>
2026-05-14 05:58:27 -0500

    replaced combined data in teams-data and created owners.js and teams.js and had more in depth data

---
+++ owners.js
@@ -0,0 +1,272 @@
+export const owners = [
+    {
+        id: "owner-1",
+        name: "Dominic Bernal",
+        current: false,
+        activeYears: [
+            2013,
+            2014,
+            2015
+        ]
+    },
+    {
+        id: "owner-2",
+        name: "Crystal Bade",
+        current: true,
+        activeYears: [
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023,
+            2024,
+            2025
+        ]
+    },
+    {
+        id: "owner-3",
+        name: "Aaron Bade",
+        current: true,
+        activeYears: [
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023,
+            2024,
+            2025
+        ]
+    },
+    {
+        id: "owner-4",
+        name: "Ben Summers",
+        current: false,
+        activeYears: [
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018
+        ]
+    },
+    {
+        id: "owner-5",
+        name: "Jared Bade",
+        current: true,
+        activeYears: [
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023,
+            2024,
+            2025
+        ]
+    },
+    {
+        id: "owner-6",
+        name: "Ashley Riley",
+        current: true,
+        activeYears: [
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023,
+            2024,
+            2025
+        ]
+    },
+    {
+        id: "owner-7",
+        name: "Bryce Bade",
+        current: true,
+        activeYears: [
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023,
+            2024,
+            2025,
+        ]
+    },
+    {
+        id: "owner-8",
+        name: "Zach Schlichting",
+        current: true,
+        activeYears: [
+            2014,
+            2025
+        ]
+    },
+    {
+        id: "owner-9",
+        name: "Andrew Drape",
+        current: true,
+        activeYears: [
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023,
+            2024,
+            2025
+        ]
+    },
+    {
+        id: "owner-10",
+        name: "Robert Snook",
+        current: true,
+        activeYears: [
+            2015,
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023,
+            2024,
+            2025
+        ]
+    },
+    {
+        id: "owner-11",
+        name: "Kirk Riley",
+        current: false,
+        activeYears: [
+            2015,
+            2019
+        ]
+    },
+    {
+        id: "owner-12",
+        name: "Cassie McAnallen",
+        current: true,
+        activeYears: [
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023,
+            2024,
+            2025
+        ]
+    },
+    {
+        id: "owner-13",
+        name: "Cayt McAnallen",
+        current: true,
+        activeYears: [
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023,
+            2024,
+            2025
+        ]
+    },
+    {
+        id: "owner-14",
+        name: "Scott Link",
+        current: false,
+        activeYears: [
+            2020,
+            2021
+        ]
+    },
+    {
+        id: "owner-15",
+        name: "Amy Bade",
+        current: false,
+        activeYears: [
+            2019,
+            2020,
+            2021,
+            2022,
+            2023,
+            2024
+        ]
+    },
+    {
+        id: "owner-16",
+        name: "Marisa Snook",
+        current: false,
+        activeYears: [
+            2020,
+            2021,
+        ]
+    },
+    {
+        id: "owner-17",
+        name: "Christina Bade",
+        current: false,
+        activeYears: [
+            2021,
+            2022,
+            2023,
+            2024
+        ]
+    },
+    {
+        id: "owner-18",
+        name: "Julie Stokes",
+        current: true,
+        activeYears: [
+            2023,
+            2024,
+            2025
+        ]
+    },
+    {
+        id: "owner-19",
+        name: "Bryce Bade Jr",
+        current: true,
+        activeYears: [
+            2025
+        ]
+    },
+]
