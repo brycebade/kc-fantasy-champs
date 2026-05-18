7bcf3241a98ca87ce471adb0159db8ba17f38375
brycebade <bryce.bade@gmail.com>
2026-05-18 06:09:55 -0500

    refactor mobile layout. remove table on mobile and created draft cards to fix layout

--- src/pages/team.js
+++ src/pages/team.js
@@ -6,6 +6,7 @@ renderTeamsDropdown()
 
 const teamName = document.getElementById("teamName")
 const draftTableBody = document.getElementById("draftTableBody")
+const draftCards = document.getElementById("draftCards")
 
 const params = new URLSearchParams(window.location.search)
 const teamSlug = params.get("team")
@@ -24,15 +25,35 @@ teamName.textContent = `${selectedTeam.currentName} Draft Results`
 
 teamPicks.forEach((pick) => {
     const row = document.createElement("tr")
+    const card = document.createElement("div")
 
     row.innerHTML = `
-        <td class="px-4 py-3">${pick.round}</td>
-        <td class="px-4 py-3">${pick.pick}</td>
-        <td class="px-4 py-3">${pick.overall}</td>
-        <td class="px-4 py-3 font-semibold whitespace-nowrap">${pick.player}</td>
-        <td class="px-4 py-3">${pick.position}</td>
-        <td class="px-4 py-3">${pick.nflTeam}</td>
+        <td class="text-center px-4 py-3">${pick.round}</td>
+        <td class="text-center px-4 py-3">${pick.pick}</td>
+        <td class="text-center px-4 py-3">${pick.overall}</td>
+        <td class="text-center px-4 py-3 font-semibold whitespace-nowrap">${pick.player}</td>
+        <td class="text-center px-4 py-3">${pick.position}</td>
+        <td class="text-center px-4 py-3">${pick.nflTeam}</td>
     `
     
     draftTableBody.appendChild(row)
+
+    card.innerHTML = `
+    <div class="card-body p-4">
+        <div class="flex justify-between items-start gap-3">
+            <div>
+                <h2 class="card-title text-lg leading-tight:>${pick.player}</h2>
+                <p class="text-sm opacity-70">${pick.position} • ${pick.nflTeam}</p>
+            </div>
+
+            <div class="text-right text-sm">
+                <p><span class=font-bold">Round:</span> ${pick.round}</p>
+                <p><span class=font-bold">Pick:</span> ${pick.pick}</p>
+                <p><span class=font-bold">Overall:</span> ${pick.overall}</p>
+            </div>
+        </div>
+    </div>
+    `
+
+    draftCards.appendChild(card)
 })
